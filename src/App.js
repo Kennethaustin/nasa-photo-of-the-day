1d44457841b56343ba30e313e2ce5639ec69f0f3
unknown <kenneth.austin93@gmail.com>
2019-07-17 17:38:54 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [NasaData, setNasaData] = useState("https://api.nasa.gov/planetary/apod?api_key=QgbkPgyO0M5SCoeiV7M2Vp741LDB6R4QjYVxGbiT")
+  axios.get(NasaData)
+  .then(Response => {console.log(Response);
+  })
   return (
     <div className="App">
       <p>
